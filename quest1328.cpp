94b51e6cfe7d6d695e6e278bfbbcfc0def10ad07
GBruzzi <gbruzzi@sga.pucminas.br>
2023-11-26 22:51:16 -0300

    start project

---
+++ quest1328.cpp
@@ -0,0 +1,45 @@
+// https://codeforces.com/problemset/problem/1328/A
+
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ll MOD = 998244353;
+const ll MODhash = 41083620229;
+const ll NO_OPERTATION = -1;
+const double eps = 1e-8;
+const int MAX = 3000000;
+const int LOG = 20;
+
+int main()
+{
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+    ll a;
+    cin >> a;
+    ll b;
+    cin >> b;
+    ll result = 0;
+    if (a % b == 0)
+    {
+      cout << result << endl;
+    }
+    else
+    {
+      result = b - (a % b);
+      cout << result << endl;
+    }
+  }
+
+  return 0;
+}
---
+++ quest1901.cpp
@@ -0,0 +1,55 @@
+// https://codeforces.com/problemset/problem/1901/A
+
+
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ll MOD = 998244353;
+const ll MODhash = 41083620229;
+const ll NO_OPERTATION = -1;
+const double eps = 1e-8;
+const int MAX = 3000000;
+const int LOG = 20;
+
+
+int main() {
+  int t; cin >> t;
+
+  while (t--) {
+    ll n; cin >> n;
+    ll x ; cin >> x;
+
+    vector<ll> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+      cin >> a[i];
+    }
+
+    a.pb(x); a.pb(0);
+    sort(a.begin(), a.end());
+
+    ll maiorD = 0;
+    for (ll i = 0; i <= n ; i ++) {
+      if ((a[i + 1] - a[i]) > maiorD) {
+        maiorD = a[i + 1] - a[i];
+      }
+    }
+    if (((a[a.size() - 1] - a[a.size() - 2]) * 2) > maiorD) {
+      maiorD = ((a[a.size() - 1] - a[a.size() -  2]) * 2);
+    }
+
+    cout << maiorD << endl;
+  }
+
+  return 0;
+}
+
