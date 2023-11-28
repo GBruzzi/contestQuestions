08301424a81b61f62bd2811cf9814c1b9a15630d
GBruzzi <gbruzzi@sga.pucminas.br>
2023-11-28 16:40:33 -0300

    add 1866A

---
+++ quest1866A.cpp
@@ -0,0 +1,45 @@
+// https://codeforces.com/problemset/problem/1866/A
+
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ll MOD = 998244353;
+const ll MODhash = 41083620229;
+const ll NO_OPERTATION = -1;
+const double eps = 1e-8;
+const int MAX = 3000000;
+const int LOG = 20;
+
+int main()
+{
+  ll n;
+  cin >> n;
+
+  vector<ll> v(n);
+
+  for (ll i = 0; i < v.size(); i++)
+  {
+    cin >> v[i];
+  }
+
+  ll menorValor = 100000000;
+
+  for (ll i = 0; i < v.size(); i++)
+  {
+    if (abs(v[i]) < menorValor) {
+      menorValor = v[i];
+    }
+  }
+
+    cout << abs(menorValor);
+  
+  return 0;
+}
