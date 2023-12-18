657dc929c8742ffd7929892437f59db283d058b3
GBruzzi <gbruzzi@sga.pucminas.br>
2023-12-18 19:53:17 -0300

    add 10300

---
+++ UVA/10300.cpp
@@ -0,0 +1,35 @@
+// https://vjudge.csgrandeur.cn/problem/UVA-10300
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+  int t;
+  cin >> t;
+  int res = 0;
+
+  while (t--)
+  {
+
+    int f;
+    cin >> f;
+
+    while (f--)
+    {
+      ll size;
+      cin >> size;
+      ll n;
+      cin >> n;
+      ll index;
+      cin >> index;
+
+      res += size * index  ;
+    }
+  }
+
+  cout << res << endl;
+
+  return 0;
+}
