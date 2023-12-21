d09dbfe3cc39926963302568ffa3a92a2d929f52
GBruzzi <gbruzzi@sga.pucminas.br>
2023-12-20 22:40:08 -0300

    ADD 12478

---
+++ UVA/12478.cpp
@@ -0,0 +1,12 @@
+// https://vj.csgrandeur.cn/60bc2bdf9207e9857ff008758ab4eccb?v=1703033548
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+  cout << "RAKIBUL" << endl;
+
+  return 0;
+}
