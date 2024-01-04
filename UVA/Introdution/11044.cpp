330872915a534f909ba201a322f2a19ecca85d74
GBruzzi <gbruzzi@sga.pucminas.br>
2024-01-03 22:41:23 -0300

    add 11044

---
+++ UVA/Introdution/11044.cpp
@@ -0,0 +1,34 @@
+// https://vjudge.net/problem/UVA-11044
+
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ll MOD = 998244353;
+const ll MODhash = 41083620229;
+const ll NO_OPERTATION = -1;
+const double eps = 1e-8;
+const int MAX = 3000000;
+const int LOG = 20;
+
+
+int main() {
+ int t; cin >> t;
+
+ while (t--) {
+  int n; cin >> n;
+  int m; cin >> m;
+
+  cout << (n / 3) * (m / 3) << endl;
+
+ }
+
+  return 0;
+}
