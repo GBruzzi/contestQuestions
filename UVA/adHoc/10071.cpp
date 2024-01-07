e2613f4a92ea682ea455f31ab73b03cad69318b9
GBruzzi <gbruzzi@sga.pucminas.br>
2024-01-07 17:05:19 -0300

    add 10071

---
+++ UVA/adHoc/10071.cpp
@@ -0,0 +1,37 @@
+// https://vjudge.net/problem/UVA-10071
+
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+#define read(x)        \
+  for (auto &qw : (x)) \
+    cin >> qw;
+
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const ll MOD = 998244353;
+const ll MODhash = 41083620229;
+const ll NO_OPERTATION = -1;
+const double eps = 1e-8;
+const int MAX = 3000000;
+const int LOG = 20;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int v, t;
+
+  while (cin >> v >> t) {
+    ll res = v * 2 * t;
+    cout << res << endl;  
+  }
+
+   return 0;
+}
