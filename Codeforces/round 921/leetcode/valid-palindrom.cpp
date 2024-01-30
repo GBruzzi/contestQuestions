fef497b27ecc027f113edba22d8af22e06a78957
GBruzzi <gbruzzi@sga.pucminas.br>
2024-01-29 22:10:42 -0300

    valid palindrom

--- Codeforces/round 921/leetcode/valid-palindrom.cpp
+++ Codeforces/round 921/leetcode/valid-palindrom.cpp
@@ -1,56 +1,30 @@
-#include <bits/stdc++.h>
-#define f first
-#define s second
-#define pb push_back
-#define read(x)        \
-  for (auto &qw : (x)) \
-    cin >> qw;
+// https://leetcode.com/problems/valid-palindrome/
 
 using namespace std;
-typedef long long ll;
-typedef pair<ll, ll> pii;
+#include <bits/stdc++.h>
 
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3fll;
-const ll MOD = 998244353;
-const ll MODhash = 41083620229;
-const ll NO_OPERTATION = -1;
-const double eps = 1e-8;
-const int MAX = 3000000;
-const int LOG = 20;
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string sForm;
 
-int main()
-{
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+        for (char c : s) {
+            if ((tolower(c) >= 'a' && tolower(c) <= 'z') || (c >= '0' && c <= '9')
+) {
+                sForm += tolower(c);
+            }
+        }
 
-  string s = "0P";
-  string sForm;
+        int j = sForm.size() - 1;
+        for (int i = 0; i < sForm.size(); i++) {
+            
+            if (sForm[i] != sForm[j]) {
+                return false;
+            }
 
-  for (char c : s)
-  {
+            j--;
+        }
 
-    if (tolower(c) >= 'a' && tolower(c) <= 'z')
-    {
-      sForm += tolower(c);
+        return true;
     }
-  }
-
-  int j = sForm.size() - 1;
-  for (int i = 0; i < sForm.size(); i++)
-  {
-    
-    if (sForm[i] != sForm[j])
-    {
-      cout << "false";
-      return 0;
-    }
-    j--;
-  }
-
-  cout << "true";
-  cout << endl;
-  cout << sForm;
-
-  return 0;
-}
+};
