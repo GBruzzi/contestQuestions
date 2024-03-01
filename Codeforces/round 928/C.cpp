e6e03204c7bb8158dab1e8ae3b47bb0682915edf
GBruzzi <gbruzzi@sga.pucminas.br>
2024-02-29 22:19:24 -0300

    add c 928

--- Codeforces/round 928/C.cpp
+++ Codeforces/round 928/C.cpp
@@ -1,39 +1,34 @@
 // https://codeforces.com/contest/1926/problem/C
 
 #include <bits/stdc++.h>
-#define f first
-#define s second
-#define pb push_back
-#define read(x)        \
-  for (auto &qw : (x)) \
-    cin >> qw;
-
+ 
 using namespace std;
-typedef long long ll;
-typedef pair<ll, ll> pii;
-
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3fll;
-const ll MOD = 998244353;
-const ll MODhash = 41083620229;
-const ll NO_OPERTATION = -1;
-const double eps = 1e-8;
-const int MAX = 3000000;
-const int LOG = 20;
-
-int main()
-{
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int t;
-  cin >> t;
-
-  while (t--)
-  {
-    int n; cin >> n;
-
-    
-  }
-
-  return 0;
+ 
+const int MAX = 200'007;
+const int MOD = 1'000'000'007;
+ 
+int res[MAX];
+ 
+int S(int x) {
+	int res = 0;
+	while (x) {
+		res += (x % 10);
+		x /= 10;
+	}
+	return res;
+}
+ 
+void solve() {
+	int x;
+	cin >> x;
+	cout << res[x] << '\n';
+}
+ 
+int main() {
+	res[0] = 0;
+	for (int i = 1; i < MAX; i++) {
+		res[i] = res[i - 1] + S(i);
+	}
+	int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+	// solve();
 }
