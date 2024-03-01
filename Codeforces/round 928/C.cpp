b31777baea955009992d6806be5adba0a8e9238e
GBruzzi <gbruzzi@sga.pucminas.br>
2024-03-01 00:11:19 -0300

    change c

--- Codeforces/round 928/C.cpp
+++ Codeforces/round 928/C.cpp
@@ -1,34 +1,40 @@
 // https://codeforces.com/contest/1926/problem/C
 
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-const int MAX = 200'007;
-const int MOD = 1'000'000'007;
- 
-int res[MAX];
- 
-int S(int x) {
-	int res = 0;
-	while (x) {
-		res += (x % 10);
-		x /= 10;
-	}
-	return res;
+typedef long long ll;
+
+ll res[200007];
+
+ll S(ll x)
+{
+  ll sum = 0;
+  while (x > 0)
+  {
+    ll digit = x % 10;
+    sum += digit;
+    x /= 10;
+  }
+
+  return sum;
 }
- 
-void solve() {
-	int x;
-	cin >> x;
-	cout << res[x] << '\n';
-}
- 
-int main() {
-	res[0] = 0;
-	for (int i = 1; i < MAX; i++) {
-		res[i] = res[i - 1] + S(i);
-	}
-	int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
-	// solve();
+
+int main()
+{
+  res[0] = 0;
+
+  for (ll i = 1; i < 200007; i++)
+  {
+    res[i] = res[i - 1] + S(i);
+  }
+
+  ll t;
+  cin >> t;
+  while (t--)
+  {
+    ll x;
+    cin >> x;
+    cout << res[x] << endl;
+  }
 }
