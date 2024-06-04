7cd266801c6788a7e0e21c71cef9cf16b91bddc3
GBruzzi <gbruzzi@sga.pucminas.br>
2024-06-04 19:27:30 -0300

    add c 944

--- Codeforces/round 944/c.cpp
+++ Codeforces/round 944/c.cpp
@@ -16,8 +16,22 @@ int main() {
   while (t--) {
     int a,b,c,d;
     cin >> a >> b >> c >> d;
+    string res;
 
-    
+    for (int i = 1; i <= 12; i ++) {
+      if(i == a || i == b)
+        res += "a";
+      
+      if(i == c || i == d)
+        res +="b";
+    }
+
+    if (res == "abab" || res == "baba") {
+      cout << "YES" << endl;
+      continue;
+    }
+
+    cout << "NO" << endl;
 
   }
 
